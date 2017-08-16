8108d192e1559c96c058f429d8a375849001ea94
Eswaran rathinam <Eswaran rathinam>
2017-08-16 16:38:42 -0700

    added selection sort files

---
+++ src/sorting/selectionSort.java
@@ -0,0 +1,40 @@
+package sorting;
+
+import java.util.Scanner;
+
+/**
+ * Created by Eswaran on 16-Aug-17.
+ */
+public class selectionSort
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out .println("please enter the size of the array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("enter the elements of the array one by one and press enter");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int minimum=0,minindex=0;
+        for(int i=0,j=i+1; i<n;i++,j=i+1)
+        {
+            minimum=arr[i];
+            minindex=i;
+            while(j<n) {
+                if (arr[j] < minimum) {
+                    minimum = arr[j];
+                    minindex = j;
+                }j++;
+            }
+            arr[minindex]=arr[i];
+            arr[i]=minimum;
+        }
+        for(int i:arr)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
