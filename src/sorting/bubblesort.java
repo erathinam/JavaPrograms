2e48c16d564ec0f4fe00bd378fcb1b8fbec78e39
Eswaran rathinam <Eswaran rathinam>
2017-08-18 07:40:03 -0700

    modified bubble sort

--- src/sorting/bubblesort.java
+++ src/sorting/bubblesort.java
@@ -15,9 +15,9 @@ public class bubblesort {
         arr[k]=sc.nextInt();
     }
     int temp;
-    for(int i=0;i<n-2;i++)
+    for(int i=0;i<n-1;i++)
     {
-        for(int j=0;j<n-i;j++)
+        for(int j=0;j<n-i-1;j++)
         {
             if(arr[j]>arr[j+1]) {
                 temp = arr[j];
